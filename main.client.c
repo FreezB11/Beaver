068654c9a0a6644a6ba2d75e5df523f94b5482a0
FreezB11 <yashrajrao4@gmail.com>
2026-04-13 05:49:36 +0530

    just did the test with the beaver packet

--- main.client.c
+++ main.client.c
@@ -4,17 +4,19 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "beaver/b.packet.h"
+#include "beaver/b.send.h"
+#include "beaver/b.recv.h"
+
 int main() {
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    struct sockaddr_in server;
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(9090);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    char *msg = "Hello from client";
-
-    sendto(fd, msg, strlen(msg), 0,
-           (struct sockaddr*)&server, sizeof(server));
+    struct sockaddr_in dst = {0};
+    dst.sin_family = AF_INET;
+    dst.sin_port   = htons(9090);
+    inet_pton(AF_INET, "127.0.0.1", &dst.sin_addr);
+    const char *msg = "hello from beaver";
+    uint32_t seq = 1000;   // would be random in T6
+    beaver_send(fd, &dst, seq, 0, 0,
+                       (const uint8_t *)msg, strlen(msg));
+    close(fd);
 }
--- main.server.c
+++ main.server.c
@@ -2,27 +2,32 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "beaver/b.packet.h"
+#include "beaver/b.send.h"
+#include "beaver/b.recv.h"
 
 int main() {
+        // RECEIVER MODE
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in local = {0};
+    local.sin_family      = AF_INET;
+    local.sin_port        = htons(9090);
+    local.sin_addr.s_addr = INADDR_ANY;
+    bind(fd, (struct sockaddr *)&local, sizeof(local));
+    printf("[receiver] waiting on port 9090...\n");
 
-    struct sockaddr_in server, client;
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(9090);
-    server.sin_addr.s_addr = INADDR_ANY;
-
-    bind(fd, (struct sockaddr*)&server, sizeof(server));
-
-    char buff[1024];
-    socklen_t len = sizeof(client);
-
-    int n = recvfrom(fd, buff, sizeof(buff), 0,
-                     (struct sockaddr*)&client, &len);
-
-    buff[n] = '\0';
-
-    printf("Received: %s\n", buff);
+    while (1) {
+        header_t hdr;
+        uint8_t payload[MAX_PAYLOAD];
+        struct sockaddr_in src;
+        if (beaver_recv(fd, &src, &hdr, payload) == 0) {
+            if (hdr.len > 0) {
+                payload[hdr.len] = '\0';
+                printf("[receiver] payload: \"%s\"\n\n", payload);
+            }
+        }
+    }
+    close(fd);
 }
-
-///@note we can use accept and listen that is TCP protocol
