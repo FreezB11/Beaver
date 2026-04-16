b60f2bb056ea453be1502eb10698db00ff0806f8
FreezB11 <yashrajrao4@gmail.com>
2026-04-17 04:37:21 +0530

    hopefully this is what i am able to achieve when i finish or maybe better

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main(){
 
-    int fd = beaver_socket(IPV4); // obvio this udp so yea
+    // int fd = beaver_socket(IPV4); // obvio this udp so yea
+
+    beaver cli;
+    beaver_init(cli, IPV4);
+    cli.establish(IPV4, 8080, "8.8.8.8");
+
+    byte data[1024];
+    cli.send_stream(&data, sizeof(data))
     
     return 0;
 }
